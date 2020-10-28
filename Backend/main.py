d7ea2f62015eb6be89b01df1eb5334e5a023dc3b
zofia.durys <zofia.durys@stazysta.comarch.pl>
2020-10-28 13:26:16 +0100

    update

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,6 @@
+import flask
 from flask import Flask
+
 
 from Backend.csv_to_json import csv_to_json
 
@@ -8,6 +10,12 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def start():
     csv_to_json()
+
+
+@app.route("/", methods=["GET"])
+def get_data():
+    json_data = flask.request.json
+    return "JSON value sent: " + json_data
 
 
 # Run
